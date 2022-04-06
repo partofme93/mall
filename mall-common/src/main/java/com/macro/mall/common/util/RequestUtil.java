303a25856095a5934b952a82a35fef93a4373a9b
wunanfang <zhuifeng@douyu.tv>
2022-04-06 19:46:57 +0800

    代码格式调整

--- mall-common/src/main/java/com/macro/mall/common/util/RequestUtil.java
+++ mall-common/src/main/java/com/macro/mall/common/util/RequestUtil.java
@@ -25,7 +25,7 @@ public class RequestUtil {
         if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
             // 从本地访问时根据网卡取本机配置的IP
-            if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
+            if ("127.0.0.1".equals(ipAddress) || "0:0:0:0:0:0:0:1".equals(ipAddress)) {
                 InetAddress inetAddress = null;
                 try {
                     inetAddress = InetAddress.getLocalHost();
